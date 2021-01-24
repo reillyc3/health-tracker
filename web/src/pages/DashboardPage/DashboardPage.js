bb6be5c64e1d7c1f3414e7ee42c27ec061d2932e
Connor Reilly <connorreilly@MBP-Creilly.attlocal.net>
2021-01-24 16:02:39 -0600

    table view added to entry cell

--- web/src/pages/DashboardPage/DashboardPage.js
+++ web/src/pages/DashboardPage/DashboardPage.js
@@ -5,7 +5,7 @@ import HealthPostsCell from 'src/components/HealthPostsCell'
 const DashboardPage = () => {
   return (
     <NavLayout>
-      <div className="flex justify-center">
+      <div className="flex justify-center py-10">
         <Link
           className="border-2 border-blue-500 rounded-full text-blue-500 px-4 py-3 transition duration-300 ease-in-out hover:bg-blue-500 hover:text-white mr-6"
           to={routes.newPost()}
--- web/src/pages/HomePage/HomePage.js
+++ web/src/pages/HomePage/HomePage.js
@@ -10,9 +10,9 @@ const HomePage = () => {
           <section className="">
             <div class="flex flex-col md:flex-row items-center pt-32 pb-16 md:pb-0">
             <div class="md:w-1/2 lg:w-1/3 mb-4 sm:mb-16 md:mb-0">
-          <h2 class="text-xl font-bold text-secondary-600 uppercase mb-2">Make the change</h2>
+          <h2 class="text-blue-500 text-xl font-bold text-secondary-500 uppercase mb-2">Make the change</h2>
           <h1 class="text-4xl md:text-5xl font-bold text-secondary leading-tight mb-6 md:mb-10">Start Tracking Your Health</h1>
-          <a href="#" class="bg-primary px-0 md:px-0 py-3 md:py-4 text-lg md:text-xl text-secondary font-bold uppercase rounded hover:bg-primary-400">Sign Up Today</a>
+          <a href="#" class="text-blue-500 bg-primary px-0 md:px-0 py-3 md:py-4 text-lg md:text-xl text-secondary font-bold uppercase rounded hover:bg-primary-400">Sign Up Today</a>
         </div>
         <div class="mt-16 sm:mt-0 flex-1 flex justify-end">
           <img calss="scale-100" src="../src/images/undraw_healthy.svg"></img>
